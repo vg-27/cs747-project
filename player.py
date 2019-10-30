254fb4bf6c798387e89d13fc0c41a2d32d057494
aslimaye <41477314+aslimaye@users.noreply.github.com>
2019-10-30 17:30:50 +0530

    removing errors

--- player.py
+++ player.py
@@ -22,14 +22,15 @@ class player:
 		x = int(state/n)
 		y = state%n;
 		[x1,y1,reward,finished]=self.grid.take_action(x,y,action)
-		reward = reward + self.reward_self(x,y,x1,y1,self)
+		reward = reward + self.reward_self(x,y,x1,y1)
 		return int(x1*n+y1),reward,finished
 
-	def reward_self(x,y,x1,y1,self):
-		return self.phi(x,y,self.grid) - self.phi(x1,y1,self.grid)
+	def reward_self(self,x,y,x1,y1):
+		return self.phi(x,y) - self.phi(x1,y1)
 
-	def phi(x,y,grid):
-		return math.sqrt((x-grid.finish[0])^2 + (y-grid.finish[1])^2)
+	def phi(self,x,y):
+		return 3
+		# return math.sqrt((x-self.grid.finish[0])^2 + (y-self.grid.finish[1])^2)
 
 	def reset(self):
 		self.grid.reset()
